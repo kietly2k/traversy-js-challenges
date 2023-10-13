5ee09a1fa1ac6a1c1c0253f803aad1e8824e57d3
Kiet Ly <kietly2k@gmail.com>
2023-10-13 17:57:11 +0700

    finish challenge 2.1, 2.2

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    let arrFizzBuzz = [];
+    for (let i = 1; i <= num; i++) {
+        let fizzBuzz = "";
+        if (i % 3 == 0) {
+            fizzBuzz += "Fizz";
+        }
+        if (i % 5 == 0) {
+            fizzBuzz += "Buzz";
+        }
+
+        arrFizzBuzz.push(fizzBuzz !== "" ? fizzBuzz : i);
+    }
+    return arrFizzBuzz;
+}
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    let arrResult = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let index = arr2.indexOf(arr1[i]);
+        if (index > -1) {
+            arrResult.push(arr1[i]);
+        }
+    }
+    return arrResult;
+}
 
 module.exports = arrayIntersection;
