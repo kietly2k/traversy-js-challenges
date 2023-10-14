65f7dd5f1c8da677d55e479cc90688cdaae9c9c3
Kiet Ly <kietly2k@gmail.com>
2023-10-14 18:18:30 +0700

    finish 02. basic challenge

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  let arrResult = [];
+  
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = roll();
+    const dice2 = roll();
+    const sum = dice1 + dice2;
+    let result = "roll again";
+
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    }
+
+    if (sum === 2 || sum === 3 || sum === 12) {
+      result = "lose";
+    }
+    
+    arrResult.push({ dice1: dice1, dice2: dice2, sum: dice1 + dice2, result: result });
+  }
+  return arrResult;
+}
+
+function roll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
 module.exports = diceGameSimulation;
--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,24 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+  let num1 = "";
+  let num2 = "";
+  let num3 = "";
+
+  for (let i = 0; i < 3; i++) {
+    num1 += numbers[i];
+  }
+  phone = `(${num1})`;
+
+  for (let i = 3; i < 6; i++) {
+    num2 += numbers[i];
+  }
+  phone += ` ${num2}`;
+
+  for (let i = 6; i < numbers.length; i++) {
+    num3 += numbers[i];
+  }
+
+  phone += `-${num3}`;
+  return phone;
+}
 
 module.exports = formatPhoneNumber;
