7c6fc8f2fd747fe0c22b5187f089a50103cfa8c0
Kiet Ly <kietly2k@gmail.com>
2023-10-19 18:06:38 +0700

    finish 6.4, 6.5

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,25 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+    const map = new Map([]);
+    const arr = str
+        .toLowerCase()
+        .replace(/[^\w\s]/gi, "")
+        .split(" ");
+
+    arr.forEach((text) => {
+        if (text !== "") {
+            // Check if map already have the value then increase the time
+            if (map.has(text)) {
+                const value = map.get(text);
+                map.set(text, value + 1);
+            }
+            // Ortherwise insert new key value item to map
+            else {
+                map.set(text, 1);
+            }
+        }
+    });
+
+    return map;
+}
 
 module.exports = wordFrequencyCounter;
--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+    const directory = new Map();
+
+    for (const num of phoneNumbers) {
+        const arrNum = num.split(":");
+        directory.set(arrNum[0], arrNum[1]);
+    }
+
+    return directory;
+}
 
 module.exports = phoneNumberDirectory;
